6caf057fadefbd4b674d993cbb5e0e3fcf4fc057
SHAIKH1234-sketch <85006859+SHAIKH1234-sketch@users.noreply.github.com>
2023-05-06 00:32:52 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER='innerheartfight@gmail.com'
-EMAIL_HOST_PASSWORD='Ayan@111'
+EMAIL_HOST_PASSWORD='xxxx'
 EMAIL_USE_TLS = True
